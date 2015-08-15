e7d2f4271e31af9006b79eda94ab1e0e860bacfb
Bodie Sullivan <bodiesullivan@gmail.com>
2015-08-15 18:47:01 -0500

    Minor changes
    Hooked up template type styles to dropdown selection.

--- js/generateTemplateController.js
+++ js/generateTemplateController.js
@@ -9,15 +9,17 @@ myApp.controller('generateTemplateController', ['$scope',
       $scope.templateInput.addCustomItem();
 
       $scope.generatedTemplate = '';
+
       $scope.buildTemplate = buildTemplate;
 
       function buildTemplate() {
         $scope.generatedTemplate =   $scope.templateInput.buildTemplate();
-  // &{template:5eDefault} {{title=Mayfield}} {{subheader=Small Town}} {{subheaderright=Forest}} {{subheader2=Population: 78}} {{showclassactions=1}} {{Government=Council}} {{Hospitality=Trusting}} {{Tavern=Spring Breeze}} {{Quest=A long harsh winter has worn down the town's supplies and morale.}}
+        /**
+        * Ecample Macro:
+        * &{template:5eDefault} {{title=Mayfield}} {{subheader=Small Town}} {{subheaderright=Forest}} {{subheader2=Population: 78}} {{showclassactions=1}} {{Government=Council}} {{Hospitality=Trusting}} {{Tavern=Spring Breeze}} {{Quest=A long harsh winter has worn down the town's supplies and morale.}}
+        **/
+        //
       }
 
-      // function addCustomItem () {
-      //   $scope.templateInput.addCustomItem();
-      // }
   }]
 );
